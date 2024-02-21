cd321c2daec6fca31813b31701f38160ff4ff3f5
Hans Naert <hans.naert@vives.be>
2024-02-21 15:34:11 +0100

    show cputemperature

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Iot.Device.CpuTemperature;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var rpiCpuTemp = new CpuTemperature();
+if(rpiCpuTemp.IsAvailable)
+   Console.WriteLine($"The cpu temp is {rpiCpuTemp.Temperature.DegreesCelsius}");
